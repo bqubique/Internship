4cedd5d89292f2af98b06f5651f1b8cd9f1b25e5
bqubique <blinnqipa@gmail.com>
2021-08-10 22:42:01 +0200

    Add addTextChangedListener to the EditText.

--- app/src/main/java/com/bqubique/internship/view/MainActivity.kt
+++ app/src/main/java/com/bqubique/internship/view/MainActivity.kt
@@ -2,8 +2,11 @@ package com.bqubique.internship.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import com.bqubique.internship.R
 import com.bqubique.internship.service.MovieService
 import kotlinx.coroutines.GlobalScope
@@ -14,12 +17,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById<Button>(R.id.button)
+        val button: Button = findViewById(R.id.button)
+        val etMovieName: EditText = findViewById(R.id.etMovieName)
+
+        etMovieName.addTextChangedListener(
+            object: TextWatcher{
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    if(count %  3 ==0){
+                        Log.d("MAINACT",s.toString());
+                    }
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+//                    Log.d("MAINACT",s.toString());
+                }
+
+            }
+        )
         button.setOnClickListener {
-            Log.d("MAINACT", "HEMLOOOOOO")
             GlobalScope.launch {
                 val response = MovieService().api.getMovies()
-                Log.d("MAINACT", response.toString())
+                Log.d("MAINACT", response.title)
             }
         }
     }
