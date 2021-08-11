3da5b326edfbe592821720c6c244e12894d44fa5
bqubique <blinnqipa@gmail.com>
2021-08-11 13:44:10 +0200

    Add Material Design Text Field. Add movie search on every 3rd character entered in the field.

--- app/src/main/java/com/bqubique/internship/view/MainActivity.kt
+++ app/src/main/java/com/bqubique/internship/view/MainActivity.kt
@@ -26,31 +26,7 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
 //        val button: Button = findViewById(R.id.button)
-//        val etMovieName: EditText = findViewById(R.id.etMovieName)
-//
-//        etMovieName.addTextChangedListener(
-//            object : TextWatcher {
-//                override fun beforeTextChanged(
-//                    s: CharSequence?,
-//                    start: Int,
-//                    count: Int,
-//                    after: Int
-//                ) {
-//
-//                }
-//
-//                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-//                    if (count % 3 == 0) {
-//                        Log.d("MAINACT", s.toString());
-//                    }
-//                }
-//
-//                override fun afterTextChanged(s: Editable?) {
-////                    Log.d("MAINACT",s.toString());
-//                }
-//
-//            }
-//        )
+
 //        button.setOnClickListener {
 //            GlobalScope.launch {
 //                val response = MovieService().api.getMovies(query = etMovieName.text.toString())
--- app/src/main/java/com/bqubique/internship/view/SearchMovieFragment.kt
+++ app/src/main/java/com/bqubique/internship/view/SearchMovieFragment.kt
@@ -1,6 +1,9 @@
 package com.bqubique.internship.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,28 +12,74 @@ import android.widget.EditText
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import com.bqubique.internship.R
+import com.bqubique.internship.service.MovieService
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.textfield.TextInputEditText
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class SearchMovieFragment : Fragment() {
+    private lateinit var textField: TextInputEditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search_movie, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val etSearchMovie: EditText = view.findViewById(R.id.etMovieSearch)
-        val fab : FloatingActionButton = view.findViewById(R.id.fabSearch)
-        fab.setOnClickListener {
-            val action: NavDirections = SearchMovieFragmentDirections.actionSearchMovieFragmentToMovieFragment()
 
+        super.onViewCreated(view, savedInstanceState)
+
+        val fab: FloatingActionButton = view.findViewById(R.id.fabSearch)
+        textField = view.findViewById(R.id.tietMovieSearch)
+
+        textField.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                if (count % 3 == 0) {
+                    Log.d("MAINACT", s.toString());
+                    GlobalScope.launch {
+                        searchMovie(s.toString())
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+//                    Log.d("MAINACT",s.toString());
+            }
+
+        })
+
+
+
+        fab.setOnClickListener {
+            val action: NavDirections =
+                SearchMovieFragmentDirections.actionSearchMovieFragmentToMovieFragment()
             Navigation.findNavController(it).navigate(action)
         }
 
+    }
+
+    fun searchMovie(movieName: String?) {
+        GlobalScope.launch {
+            val response = MovieService().api.getMovies(query = textField.text.toString())
+            Log.d("MAINACT", response.toString())
+        }
     }
 
 }
